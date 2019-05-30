109ac1d2b2be70b1dd211d3e2dc7d373c87fd98c
Jacek <21jp27@gmail.com>
2019-05-31 01:34:07 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express'); //załadowanie modułu express
+const app = express(); //utworzenie aplikacji express
+const serv = require('http').Server(app); //utworzenie serwera WWW
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+// const register = fs.readFileSync('json/register.json');
+// const users = JSON.parse(register);
+const router = express.Router();
+
+// serv.listen(2000);
+console.log("Server works...")
+// console.log(users)
+app.use('/', router);
+app.use(express.static('home'));
+app.listen(process.env.port || 2000);
+app.use(bodyParser.json());
+
+/////////////////// Ładowanie Strony /////////////////////
+
+router.get('/', function(req, res){ 
+	res.sendFile(path.join(__dirname + '/home/html/index.html'));
+});
+router.get('/log.html', function(req, res){ 
+	res.sendFile(path.join(__dirname + '/home/html/log.html'));
+});
+router.get('/filmy.html', function(req, res){ 
+	res.sendFile(path.join(__dirname + '/home/html/filmy.html'));
+});
+router.get('/kina.html', function(req, res){ 
+	res.sendFile(path.join(__dirname + '/home/html/kina.html'));
+});
+router.get('/main.js', function(req, res){ 
+	res.sendFile(path.join(__dirname + '/home/js/main.js'));
+});
+router.get('/style.css', function(req, res){ 
+	res.sendFile(path.join(__dirname + '/home/css/style.css'));
+});
+router.get('/kino.json', function(req, res){ 
+	res.sendFile(path.join(__dirname + '/home/json/kino.json'));
+});
+router.get('/reservation.json', function(req, res){ 
+	res.sendFile(path.join(__dirname + '/home/json/reservation.json'));
+});
+router.get('/register.json', function(req, res){ 
+	res.sendFile(path.join(__dirname + '/home/json/register.json'));
+});
